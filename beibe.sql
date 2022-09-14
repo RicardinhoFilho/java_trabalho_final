c7f8d98545c9dc842f6cb30b07ffdb6a87f73038
Ricardo <ricardinhogiasson16@gmail.com>
2022-09-14 16:53:42 -0300

    BackUp Banco

--- beibe.sql
+++ beibe.sql
@@ -27,7 +27,7 @@ CREATE TABLE `categorias` (
   `nome` varchar(250) NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `categorias` (
 
 LOCK TABLES `categorias` WRITE;
 /*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
-INSERT INTO `categorias` VALUES (1,'Sabonetes','2022-08-30 13:34:25'),(2,'Toalhas','2022-08-31 18:16:54'),(3,'Teste 2','2022-08-31 18:31:21');
+INSERT INTO `categorias` VALUES (1,'Sabonetes','2022-08-30 13:34:25'),(2,'Toalhas','2022-08-31 18:16:54'),(3,'Teste 2','2022-08-31 18:31:21'),(6,'Higiene','2022-09-14 12:08:15'),(8,'Teste 3','2022-09-14 12:09:30');
 /*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,12 +52,13 @@ CREATE TABLE `chamado` (
   `titulo` varchar(250) NOT NULL,
   `texto` text NOT NULL,
   `cliente_id` int NOT NULL,
-  `finalizado` tinyint(1) DEFAULT '0',
+  `finalizado` timestamp NULL DEFAULT NULL,
   `criacao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `PRODUTO_ID` int DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `FK_CHAMAD_CLIENTE` (`cliente_id`),
   CONSTRAINT `FK_CHAMAD_CLIENTE` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +67,7 @@ CREATE TABLE `chamado` (
 
 LOCK TABLES `chamado` WRITE;
 /*!40000 ALTER TABLE `chamado` DISABLE KEYS */;
-INSERT INTO `chamado` VALUES (1,'Estorno','Qurbo receber o estorno bláblávlá',1,1,'2022-08-29 11:56:03'),(2,'Teste 2','Somente um simples teste!',1,1,'2022-08-29 15:16:26'),(3,'Teste 23','Somente um simples teste!',1,0,'2022-08-29 20:48:31');
+INSERT INTO `chamado` VALUES (1,'Estorno','Qurbo receber o estorno bláblávlá',1,'2022-09-14 17:32:38','2022-08-29 11:56:03',NULL),(2,'Teste 2','Somente um simples teste!',1,'2022-09-14 17:32:45','2022-08-29 15:16:26',NULL),(3,'Teste 23','Somente um simples teste!',1,NULL,'2022-08-29 20:48:31',NULL),(4,'Teste','Deu certo se pÃ¡!',1,NULL,'2022-09-14 18:57:42',19),(5,'Mais um Chamado!','Produto Ruim!',1,NULL,'2022-09-14 19:06:03',1),(6,'Produto Muito oleoso!','Produto Ruim!',1,NULL,'2022-09-14 19:15:44',19);
 /*!40000 ALTER TABLE `chamado` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,7 +142,7 @@ CREATE TABLE `produto` (
   `saldo` int NOT NULL,
   `categoria_id` int DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -150,7 +151,7 @@ CREATE TABLE `produto` (
 
 LOCK TABLES `produto` WRITE;
 /*!40000 ALTER TABLE `produto` DISABLE KEYS */;
-INSERT INTO `produto` VALUES (1,'Produto 12',150,'2022-08-24 20:31:41','2022-08-31',500,NULL),(4,'Produto 2',13,'2022-08-24 21:06:37','2022-08-24',70,NULL);
+INSERT INTO `produto` VALUES (1,'Produto 12',150,'2022-08-24 20:31:41','2022-08-31',500,NULL),(19,'Teste',150,'2022-09-14 12:04:37','2022-09-14',250,NULL);
 /*!40000 ALTER TABLE `produto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -172,7 +173,7 @@ CREATE TABLE `resposta` (
   KEY `chamado_id` (`chamado_id`),
   CONSTRAINT `FK_RESP_FUNC` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario` (`ID`),
   CONSTRAINT `resposta_ibfk_1` FOREIGN KEY (`chamado_id`) REFERENCES `chamado` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -181,7 +182,7 @@ CREATE TABLE `resposta` (
 
 LOCK TABLES `resposta` WRITE;
 /*!40000 ALTER TABLE `resposta` DISABLE KEYS */;
-INSERT INTO `resposta` VALUES (5,'AGUARDO RESPOSTA',NULL,'2022-08-29 12:02:23',1),(6,'Olá, consegue nos informar qual produto deseja retirar?',1,'2022-08-29 12:02:30',1),(7,'AGUARDO RESPOSTA',NULL,'2022-08-29 20:49:38',2),(8,'Testte',2,'2022-08-29 21:38:10',1),(9,'Deu certo!',2,'2022-08-29 21:39:29',1),(10,'',2,'2022-08-29 21:39:41',1),(11,'',2,'2022-08-29 21:43:29',1),(12,'Teste 50',NULL,'2022-08-29 21:49:54',1),(13,'tESTE',2,'2022-08-29 21:52:11',1),(14,'Teste r',2,'2022-08-29 21:54:12',1),(15,'',2,'2022-08-29 21:54:21',1),(16,'',2,'2022-08-29 21:54:38',1),(17,'O 1 da certpo!',2,'2022-08-29 21:55:20',1),(18,'11',2,'2022-08-29 21:55:24',1),(19,'',2,'2022-08-29 21:58:53',1),(20,'Primeiro teste!',2,'2022-08-29 22:02:10',2),(21,'Segundo ja n vai aparecer!',2,'2022-08-29 22:02:20',2),(22,'Deu certro!',2,'2022-08-29 22:02:34',3),(23,'Deu certinho, q vbom!',2,'2022-08-30 10:17:11',1),(24,'Deu certo aqui!',2,'2022-08-30 12:35:38',1),(25,'Showw',2,'2022-08-30 12:45:04',2),(26,'aa',2,'2022-08-30 12:45:31',1),(27,'Terminado',2,'2022-08-30 12:45:46',2),(28,'Teste final!',2,'2022-08-30 12:50:32',1),(29,'Deu tudo certo!',2,'2022-08-30 12:50:42',2),(30,'Legal , ultimo chamado!',2,'2022-08-30 13:13:28',3),(31,'Mais uma menssagem!',2,'2022-08-31 18:03:35',3),(32,'Outro Funcionario',1,'2022-08-31 19:36:48',3),(33,'A',2,'2022-08-31 20:01:00',3),(34,'Teste',NULL,'2022-08-31 20:21:28',3),(35,'Teste',NULL,'2022-08-31 20:23:10',3),(36,'Teste',NULL,'2022-08-31 20:24:50',3),(37,'MAIS UM TESTE',NULL,'2022-08-31 20:26:05',3),(38,'mAIS UM TESTE!',NULL,'2022-08-31 20:26:27',3),(39,'Deu Certinho!',NULL,'2022-08-31 20:30:19',3),(40,'Q bom q deu certo!',2,'2022-08-31 20:34:58',3);
+INSERT INTO `resposta` VALUES (5,'AGUARDO RESPOSTA',NULL,'2022-08-29 12:02:23',1),(6,'Olá, consegue nos informar qual produto deseja retirar?',1,'2022-08-29 12:02:30',1),(7,'AGUARDO RESPOSTA',NULL,'2022-08-29 20:49:38',2),(8,'Testte',2,'2022-08-29 21:38:10',1),(9,'Deu certo!',2,'2022-08-29 21:39:29',1),(10,'',2,'2022-08-29 21:39:41',1),(11,'',2,'2022-08-29 21:43:29',1),(12,'Teste 50',NULL,'2022-08-29 21:49:54',1),(13,'tESTE',2,'2022-08-29 21:52:11',1),(14,'Teste r',2,'2022-08-29 21:54:12',1),(15,'',2,'2022-08-29 21:54:21',1),(16,'',2,'2022-08-29 21:54:38',1),(17,'O 1 da certpo!',2,'2022-08-29 21:55:20',1),(18,'11',2,'2022-08-29 21:55:24',1),(19,'',2,'2022-08-29 21:58:53',1),(20,'Primeiro teste!',2,'2022-08-29 22:02:10',2),(21,'Segundo ja n vai aparecer!',2,'2022-08-29 22:02:20',2),(22,'Deu certro!',2,'2022-08-29 22:02:34',3),(23,'Deu certinho, q vbom!',2,'2022-08-30 10:17:11',1),(24,'Deu certo aqui!',2,'2022-08-30 12:35:38',1),(25,'Showw',2,'2022-08-30 12:45:04',2),(26,'aa',2,'2022-08-30 12:45:31',1),(27,'Terminado',2,'2022-08-30 12:45:46',2),(28,'Teste final!',2,'2022-08-30 12:50:32',1),(29,'Deu tudo certo!',2,'2022-08-30 12:50:42',2),(30,'Legal , ultimo chamado!',2,'2022-08-30 13:13:28',3),(31,'Mais uma menssagem!',2,'2022-08-31 18:03:35',3),(32,'Outro Funcionario',1,'2022-08-31 19:36:48',3),(33,'A',2,'2022-08-31 20:01:00',3),(34,'Teste',NULL,'2022-08-31 20:21:28',3),(35,'Teste',NULL,'2022-08-31 20:23:10',3),(36,'Teste',NULL,'2022-08-31 20:24:50',3),(37,'MAIS UM TESTE',NULL,'2022-08-31 20:26:05',3),(38,'mAIS UM TESTE!',NULL,'2022-08-31 20:26:27',3),(39,'Deu Certinho!',NULL,'2022-08-31 20:30:19',3),(40,'Q bom q deu certo!',2,'2022-08-31 20:34:58',3),(41,'Deu certo!',NULL,'2022-09-14 16:50:42',3),(42,'Showw!',NULL,'2022-09-14 16:50:50',3),(43,'Deu boa!',1,'2022-09-14 18:59:02',3),(44,'O de baixo n funcionou!',2,'2022-09-14 18:59:41',3),(45,'Deu boa agora, o problema era o send-redirect!',2,'2022-09-14 19:02:19',3),(46,'Deu certo!',NULL,'2022-09-14 19:09:11',5),(47,'Showw!',NULL,'2022-09-14 19:13:01',4),(48,'Deu boa?',NULL,'2022-09-14 19:15:26',4),(49,'Achei este produto oleoso demais msm!',NULL,'2022-09-14 19:15:57',6);
 /*!40000 ALTER TABLE `resposta` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -194,4 +195,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-13 19:51:49
+-- Dump completed on 2022-09-14 16:53:19
